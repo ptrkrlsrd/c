5f2c47489d06ab606760fda6e306ff03aed70a08
ptrkrlsrd <ptrkrlsrd@users.noreply.github.com>
2023-08-03 14:58:53 +0200

    improve error handling

--- src/main.rs
+++ src/main.rs
@@ -3,38 +3,33 @@ use std::fs::{self, File};
 use std::io::{self, prelude::*};
 use std::path::Path;
 
-fn read_file(path: &Path) {
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(err) => panic!("couldn't open {}: {}", display, err.to_string()),
-        Ok(file) => file,
-    };
-
+fn read_file(path: &Path) -> io::Result<()> {
+    let mut file = File::open(&path)?;
     let mut file_content = String::new();
-    match file.read_to_string(&mut file_content) {
-        Err(err) => panic!("couldn't read {}: {}", display, err.to_string()),
-        Ok(_) => print!("{}", file_content),
-    }
+    file.read_to_string(&mut file_content)?;
+    println!("{}", file_content);
+    Ok(())
 }
 
-fn handle_single_argument(arg: &String) {
+fn handle_single_argument(arg: &String) -> io::Result<()> {
     let path = Path::new(arg.as_str());
     if !path.exists() {
-        return;
+        return Ok(());
     }
 
     if path.is_file() {
-        read_file(path);
+        read_file(path)?;
     } else if path.is_dir() {
-        read_directory(arg.as_str()).unwrap();
+        read_directory(arg.as_str())?;
     }
+    Ok(())
 }
 
-fn handle_multiple_arguments(args: &Vec<String>) {
-    args.into_iter().for_each(|i| {
-        handle_single_argument(i);
-    });
+fn handle_multiple_arguments(args: &[String]) -> io::Result<()> {
+    for arg in args {
+        handle_single_argument(arg)?;
+    }
+    Ok(())
 }
 
 fn handle_no_argument() -> io::Result<()> {
@@ -42,30 +37,32 @@ fn handle_no_argument() -> io::Result<()> {
     Ok(())
 }
 
-fn read_directory(path: &str) -> Result<(), io::Error> {
+fn read_directory(path: &str) -> io::Result<()> {
     let entries = fs::read_dir(path)?;
-    Ok(for entry in entries {
+    for entry in entries {
         let entry = entry?;
         let path = entry.path();
         if path.is_file() {
             println!("{}", entry.file_name().to_string_lossy());
         }
-    })
+    }
+    Ok(())
 }
 
-fn handle_args(args: env::Args) {
+fn handle_args(args: env::Args) -> io::Result<()> {
     let arg_list: Vec<String> = args.collect();
 
-    let last = arg_list.last().unwrap();
-
     match arg_list.len() {
-        1 => handle_no_argument().unwrap(),
-        2 => handle_single_argument(&last),
-        _ => handle_multiple_arguments(&arg_list[1..].to_vec()),
+        1 => handle_no_argument()?,
+        2 => handle_single_argument(&arg_list.last().unwrap())?,
+        _ => handle_multiple_arguments(&arg_list[1..])?,
     }
+    Ok(())
 }
 
 fn main() {
     let args: env::Args = env::args();
-    handle_args(args);
+    if let Err(err) = handle_args(args) {
+        eprintln!("Error: {}", err);
+    }
 }
